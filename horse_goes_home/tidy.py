9cc6de9db0ef4512a80b612e82ef416ba9298f3a
pushpops <64456870+pushpops@users.noreply.github.com>
2020-06-27 18:27:36 +0900

    Add files via upload
    calculate score, start screen, endevent

--- horse_goes_home/tidy.py
+++ horse_goes_home/tidy.py
@@ -4,10 +4,24 @@ import pandas as pd
 import csv
 import sys
 import random
+import time as tm
+import math
 
+WIDTH = 700
+HEIGHT = 500
+FPS = 30
+DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+#             R    G    B
+WHITE     = (255, 255, 255)
+BLACK     = (  0,   0,   0)
+RED       = (255,   0,   0)
+GREEN     = (  0, 255,   0)
+DARKGREEN = (  0, 155,   0)
+DARKGRAY  = ( 40,  40,  40)
+BGCOLOR = BLACK
 class Hardle:
 	pic = ''
-	x = 700
+	x = WIDTH
 	def __init__(self, pic):
 		self.pic = pic
 	def move(self):
@@ -79,19 +93,15 @@ class ScoreMaker:
 
 		return self.notes, self.is_hardle
 
-WIDTH = 700
-HEIGHT = 500
-FPS = 30
-
 def main():
-	global	WIDTH,HEIGHT,FPS,DISPLAYSURF,IMAGEDICT,FPSCLOCK,TIMING,IS_HARDLE,CORRECT,JUMP,FAIL,screen,font
+	global IMAGEDICT,FPSCLOCK,TIMING,IS_HARDLE,CORRECT,JUMP,FAIL,MISS,screen,font,BASICFONT,RESULT
 	#initialise
 	pygame.mixer.quit()
 	pygame.mixer.pre_init(buffer=64)
 	pygame.init()
 
 	FPSCLOCK = pygame.time.Clock()
-	DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+	
 	pygame.display.set_caption('tidy')
 	sm = ScoreMaker()
 	TIMING, IS_HARDLE = sm.make()
@@ -117,14 +127,17 @@ def main():
 	CORRECT.set_volume(0.2)
 	JUMP = pygame.mixer.Sound("se/Motion-pop15-1.wav")
 	FAIL = pygame.mixer.Sound("se/fail.wav")
+	MISS = pygame.mixer.Sound("se/match4.wav")
+	MISS.set_volume(0.3)
 	screen = pygame.display.set_mode((WIDTH, HEIGHT))
 	font = pygame.font.Font(None, 30)
-	
+	BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
 	while True:
-		runGame()
+		showStartScreen()
+		RESULT = runGame()
+		showEndEvent(RESULT)
 
 def runGame():
-	jumping = False    		
 	j = 0 #index for judge with TIMING
 	s = 0 #index for set hardle pictures with TIMING
 	INPUT = [0]*170	#input data from player
@@ -133,12 +146,12 @@ def runGame():
 	cx = 0 #cloud's x
 	hardles = []
 	image = IMAGEDICT['stop']
-	jumpy = 0
-	jumpt = 0
+	jump = dict(y = 0, t = 0, jumping = False)
 	time = 0
 	time0 = 0
 	dist = 0.0
 	judgement = ''
+	score = 0.0
 
 	pygame.mixer.music.play()	
 	#setting Fonts
@@ -152,40 +165,43 @@ def runGame():
 
 		#draw objects in the sky
 		cx = drawCloud(cx)
-
+		time = pygame.mixer.music.get_pos()/1000	#get bgm's play time
 		#get key event, set image, play SE, and judge
 		for event in pygame.event.get():					
-			if event.type == KEYDOWN:
-				time = pygame.mixer.music.get_pos()/1000	#get bgm's play time
-				if event.key == K_SPACE:	
-					jumping = True
+			if event.type == KEYDOWN:	
+				if event.key == K_SPACE:
+					jump['jumping'] = True
 					JUMP.play()
 					#判定	
-					screen.blit(font.render(judgement, True, (0,0,0)), [240,220])
+					j,INPUT,JUDGE,DIST,image,judgement = judge(j,time,INPUT,JUDGE,DIST,image,judgement,jump)
+					if image == IMAGEDICT['bad'] and judgement != "Bad":
+						image = IMAGEDICT['run1']
 				elif event.key == K_f or event.key == K_j:
 					if image == IMAGEDICT['stop'] or image == IMAGEDICT['run2'] or image == IMAGEDICT['bad']:
 						image = IMAGEDICT['run1']
 					elif image == IMAGEDICT['run1']:
 						image = IMAGEDICT['run2']
 					#判定
-					j,INPUT,JUDGE,DIST,image,judgement = judge(j,time,INPUT,JUDGE,DIST,image,judgement)							
+					j,INPUT,JUDGE,DIST,image,judgement = judge(j,time,INPUT,JUDGE,DIST,image,judgement,jump)							
 				elif event.key == K_ESCAPE:
 					terminate()
 			elif event.type == QUIT:
 				terminate()
-		
+
+		#音符を見逃したか判定
+		if j < len(TIMING):
+			j,INPUT,JUDGE,DIST,image,judgement = missed(j,time,INPUT,JUDGE,DIST,image,judgement)
+
 		#判定結果を表示	
 		screen.blit(font.render(judgement, True, (0,0,0)), [240,220])
-
-		#draw characters
-		image,jumpy,jumpt,jumping = drawCharacter(image,jumpy,jumpt,jumping)
-
-		#左上テキストの表示
-		text = font.render(str(time), True, (0,0,0)) #テキストの設定
-		screen.blit(text, [0,0]) #テキストの表示
+		#キャラクターを描画
+		image,jump = drawCharacter(image,jump)
 		
+		#プレイ終了
 		if pygame.mixer.music.get_pos()/1000 > 54.00:
-			return terminate()
+			for result in zip(TIMING,INPUT,DIST,JUDGE):
+				print(result)
+			return JUDGE
 		#prepare for the next frame
 		pygame.display.update()
 		FPSCLOCK.tick_busy_loop(FPS)
@@ -193,6 +209,90 @@ def runGame():
 def terminate():
     pygame.quit()
     sys.exit()
+
+def calculater(JUDGE):
+	score = 0
+	for str in JUDGE:
+		if str == 'Perfect!':
+			score += 3
+		elif str == 'Good!':
+			score += 3*0.7
+		elif str == 'Bad':
+			score += 3*0.1
+		elif str == 'Miss':
+			score += -3*0.1
+
+	score = round((score/(len(JUDGE)*3))*100,3)
+	return score
+
+def showEndEvent(JUDGE):
+	score = calculater(JUDGE)
+	print(score)
+	while True:
+		screen.blit(font.render(str(score), True, (0,0,0)), [100,100])
+		if checkForKeyPress():
+			pygame.event.get() # clear event queue
+			return
+		pygame.display.update()
+		FPSCLOCK.tick(FPS)
+
+def showStartScreen():
+    titleFont = pygame.font.Font('freesansbold.ttf', 100)
+    titleSurf1 = titleFont.render('Tempo Jigoku', True, WHITE, DARKGREEN)
+    titleSurf2 = titleFont.render('Tempo Jigoku', True, GREEN)
+
+    degrees1 = 0
+    degrees2 = 0
+    while True:
+        DISPLAYSURF.fill(BGCOLOR)
+        rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
+        rotatedRect1 = rotatedSurf1.get_rect()
+        rotatedRect1.center = (WIDTH / 2, HEIGHT / 2)
+        DISPLAYSURF.blit(rotatedSurf1, rotatedRect1)
+
+        rotatedSurf2 = pygame.transform.rotate(titleSurf2, degrees2)
+        rotatedRect2 = rotatedSurf2.get_rect()
+        rotatedRect2.center = (WIDTH / 2, HEIGHT / 2)
+        DISPLAYSURF.blit(rotatedSurf2, rotatedRect2)
+
+        drawPressKeyMsg()
+
+        if checkForKeyPress():
+            pygame.event.get() # clear event queue
+            return
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+        degrees1 += 3 # rotate by 3 degrees each frame
+        degrees2 += 7 # rotate by 7 degrees each frame
+
+def checkForKeyPress():
+    if len(pygame.event.get(QUIT)) > 0:
+        terminate()
+
+    keyUpEvents = pygame.event.get(KEYUP)
+    if len(keyUpEvents) == 0:
+        return None
+    if keyUpEvents[0].key == K_ESCAPE:
+        terminate()
+    return keyUpEvents[0].key
+
+def drawPressKeyMsg():
+    pressKeySurf = BASICFONT.render('Press a key to play.', True, DARKGRAY)
+    pressKeyRect = pressKeySurf.get_rect()
+    pressKeyRect.topleft = (WIDTH - 200, HEIGHT - 30)
+    DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
+
+def missed(J,time,INPUT,JUDGE,DIST,image,judgement):
+	if time-TIMING[J]>0.25:
+		if INPUT[J]==0:
+			judgement = "Miss"
+			JUDGE[J] = judgement
+			DIST[J] = None
+			INPUT[J] = None
+			image = IMAGEDICT['bad']
+			MISS.play()
+			J += 1
+	return J,INPUT,JUDGE,DIST,image,judgement
 
 def drawHardle(S,hardles):
 	if IS_HARDLE[S]:
@@ -220,6 +320,7 @@ def drawHardle(S,hardles):
 			del hardles[0]
 	return S,hardles
 
+
 def drawCloud(cx):
 	DISPLAYSURF.blit(IMAGEDICT['cloud'],(cx,0))
 	cx -= 3
@@ -227,26 +328,37 @@ def drawCloud(cx):
 		cx = 700
 	return cx
 
-def judge(J,time,INPUT,JUDGE,DIST,image,judgement):
+def judge(J,time,INPUT,JUDGE,DIST,image,judgement,jump):
+	correctJump = True
 	for j in range(J,len(TIMING)):
 		dist = abs(TIMING[j]-time)
-		if dist < 0.05:
+		if dist < 0.07:
+			if IS_HARDLE[j] and not jump['jumping']:
+				correctJump = False
+			elif IS_HARDLE[j] and jump['jumping']:
+				correctJump = True
+			elif not IS_HARDLE[j] and jump['jumping']:
+				correctJump = False
+			elif not IS_HARDLE[j] and not jump['jumping']:
+				correctJump = True
+
+		if dist < 0.05 and correctJump:
 			judgement = "Perfect!"
 			JUDGE[j] = judgement
 			INPUT[j] = time
 			DIST[j] = dist
 			CORRECT.play()
 			J = j+1
-			return J,INPUT,JUDGE,DIST,image,judgement
-		elif dist >= 0.05 and dist < 0.08:
+			break
+		elif 0.05 <= dist < 0.07 and correctJump:
 			judgement = "Good!"
 			JUDGE[j] = judgement
 			INPUT[j] = time
 			DIST[j] = dist
 			CORRECT.play()
 			J = j+1
-			return J,INPUT,JUDGE,DIST,image,judgement
-		elif dist >= 0.08 and dist <= 0.25:
+			break
+		elif 0.07 <= dist <= 0.25 or not correctJump:
 			judgement = "Bad"
 			JUDGE[j] = judgement
 			DIST[j] = dist
@@ -254,29 +366,30 @@ def judge(J,time,INPUT,JUDGE,DIST,image,judgement):
 			image = IMAGEDICT['bad']
 			FAIL.play()
 			J = j+1
-			return J,INPUT,JUDGE,DIST,image,judgement
+			break
 		#調べているTIMINGと入力のtimeが遠すぎたら探索を続ける
 		pygame.event.clear()
+
 	return J,INPUT,JUDGE,DIST,image,judgement
 
-def drawCharacter(image,jumpy,jumpt,jumping):
-	if jumping: #ジャンプしている
-		jumpy = -100/(0.07**2)*((jumpt-0.07)**2)+100
-		jumpt += 0.02
-		if jumpt >= 0.14:
-			jumpt = 0
-			jumpy = 0
-			jumping = False
+def drawCharacter(image,jump):
+	JUMPRATE = 10
+	JUMPHEIGHT = 100
+	if jump['jumping']: #ジャンプしている
+		jump['y'] = math.sin((math.pi / float(JUMPRATE)) * jump['t']) * JUMPHEIGHT
+		jump['t'] += 1
+		if jump['t'] >= JUMPRATE:
+			jump['t'] = 0
+			jump['y'] = 0
+			jump['jumping'] = False
 		if image == IMAGEDICT['bad']:
-			DISPLAYSURF.blit(image,(0,0-jumpy))
+			DISPLAYSURF.blit(image,(0,0-jump['y']))
 		else:
-			DISPLAYSURF.blit(IMAGEDICT['run1'],(0,0-jumpy))
+			DISPLAYSURF.blit(IMAGEDICT['run1'],(0,0-jump['y']))
+	else:DISPLAYSURF.blit(image,(0,0)) #ジャンプしていない
+	return image,jump
+	
 
-	elif image == IMAGEDICT['run1'] or image == IMAGEDICT['run2'] or image == IMAGEDICT['bad'] or image == IMAGEDICT['stop']:
-		DISPLAYSURF.blit(image,(0,0)) #ジャンプしていない
-	else: 
-		print("error: image is not proper.")
-	return image,jumpy,jumpt,jumping
 
 if __name__ == "__main__":
     main()
